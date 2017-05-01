e2d7375cefaa2f5f9c02edf99fb2eb5e74529d1f
Tatu Seppä-Lassila <tatu.seppa-lassila@weave.fi>
2017-05-01 19:59:00 +0300

    [vpc] Introduce new resources

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = "${var.cidr}"
 }
@@ -7,11 +9,27 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count  = "${length(var.public_subnets)}"
-  vpc_id = "${aws_vpc.main.id}"
+  count             = "${length(var.public_subnets)}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${element(var.public_subnets, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 }
 
 resource "aws_subnet" "private" {
+  count             = "${length(var.private_subnets)}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${element(var.private_subnets, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+}
+
+resource "aws_route_table" "private" {
   count  = "${length(var.private_subnets)}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws.vpc.main.id}"
+}
+
+resource "aws_route" "private" {
+  count = "${length(var.private_subnets)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${ID_OF_NAT_GATEWAY}"
 }
