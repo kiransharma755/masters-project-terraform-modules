52066d4e4c590929510cf1a6703c42db6cea077a
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2017-05-10 00:48:15 +0300

    [vpc] Associate route tables to subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -41,6 +41,12 @@ resource "aws_route" "public" {
   gateway_id             = "${aws_internet_gateway.main.id}"
 }
 
+resource "aws_route_table_association" "public" {
+  count          = "${length(var.public_subnets)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
 resource "aws_subnet" "private" {
   count             = "${length(var.private_subnets)}"
   vpc_id            = "${aws_vpc.main.id}"
@@ -66,6 +72,12 @@ resource "aws_route" "private" {
   route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = "${element(aws_nat_gateway.main.*.id, count.index)}"
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${length(var.private_subnets)}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
 
 # NAT resources
