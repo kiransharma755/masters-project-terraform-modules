a8f9d7ff7994ba0532ee736421f069437bc972eb
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2017-05-15 00:34:06 +0300

    Move ECS-ELB related IAM policies to app module

--- application/main.tf
+++ application/main.tf
@@ -31,11 +31,49 @@ resource "aws_ecs_service" "service" {
   cluster         = "${var.ecs_cluster}"
   task_definition = "${aws_ecs_task_definition.service_task.arn}"
   desired_count   = 2
-  iam_role        = "${var.service_iam_role}"
+  iam_role        = "${aws_iam_role.ecs_elb.arn}"
 
   load_balancer {
     elb_name       = "${aws_elb.service_elb.id}"
     container_name = "${var.name}-${var.env}"
     container_port = "${var.container_port}"
   }
+}
+
+data "aws_iam_policy_document" "ecs_elb_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_elb" {
+  name               = "${var.name}-${var.env}-ecs-elb"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_elb_assume_role.json}"
+}
+
+data "aws_iam_policy_document" "ecs_elb" {
+  statement {
+    actions = [
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:Describe*",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:RegisterTargets",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_elb" {
+  name   = "${var.name}-${var.env}-ecs-elb-policy"
+  role   = "${aws_iam_role.ecs_elb.id}"
+  policy = "${data.aws_iam_policy_document.ecs_elb.json}"
 }
