d5177c848e471ec594ea3611c7beca7d180f9c65
Tatu Seppä-Lassila <tatu.seppa-lassila@weave.fi>
2017-05-22 00:54:02 +0300

    [application] Add CIDR block to security group

--- application/main.tf
+++ application/main.tf
@@ -40,6 +40,7 @@ resource "aws_security_group_rule" "allow_all_out" {
   to_port           = 0
   from_port         = "0"
   protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.service_elb.id}"
 }
 
